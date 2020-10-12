16b472f46c93978cf1955b94a9b658801ae9a899
shadow-tricker <fengyunlingying@qq.com>
2020-10-13 00:43:48 +0800

    [add] address component

--- index.ts
+++ index.ts
@@ -1,91 +1,134 @@
+import { HistoryObject, MyHistory } from "./services/my-history";
 
-/* window.addEventListener('beforeunload', (event: UIEvent) => {
-  let message = "I'm really going to miss you if you go.";
-  event.returnValue = message;
-  return message;
-}); */
 
-/* window.addEventListener('pageshow', (e) => {
-  console.log(e);
-  console.log(1234);
-});
+const myHistory = new MyHistory();
 
-document.addEventListener('contextmenu', (e) => {
-  console.log('contextmenu');
-  appendMenu();
-  e.preventDefault();
-});
-
-function appendMenu() {
-  const ul = document.createElement('ul');
-  const li_1 = document.createElement('li');
-  li_1.innerHTML = '1';
-  const li_2 = document.createElement('li');
-  li_2.innerHTML = '2';
-  const li_3 = document.createElement('li');
-  li_3.innerHTML = '3';
-  ul.append(li_1);
-  ul.append(li_2);
-  ul.append(li_3);
-  document.body.append(ul);
-} */
-
-interface HistoryObject {
-  url: string;
-  title?: string;
-  state?: { [key: string]: any };
+interface ElementParams {
+  tagName: string;
+  children: (HTMLElement | string)[],
+  attributes?: { [key: string]: any },
+  events?: { [key: string]: (e: Event, ...params: any[]) => any }
 }
 
-class MyHistory {
-
-  private history: HistoryObject[] = [];
-
-  private currentIndex: number = -1;
-
-  get length() {
-    return this.history.length;
+function createElement({
+  tagName,
+  children,
+  attributes = {},
+  events = {}
+}: ElementParams): HTMLElement {
+  const newElem = document.createElement(tagName);
+  for (let child of children) {
+    newElem.append(child);
   }
 
-  get currentUrl() {
-    return this.currentIndex === -1
-      ? ''
-      : this.history[this.currentIndex].url;
+  const attrs = Object.keys(attributes);
+  for (let attr of attrs) {
+    newElem.setAttribute(attr, attributes[attr]);
   }
 
-  public go(link: number): void {
-    const result = this.currentIndex + link;
-    this.currentIndex = this.limited(result);
+  const evts = Object.keys(events);
+  for (let evt of evts) {
+    newElem.addEventListener(evt.toString(), events[evt]);
   }
 
-  public forward(): void {
-    this.go(1);
-  }
+  return newElem;
+}
 
-  public back(): void {
-    this.go(-1);
-  }
-
-  public pushState(urlObject: HistoryObject): void {
-    this.history = [...this.history.slice(0, this.currentIndex + 1), urlObject];
-    this.currentIndex = this.length - 1;
-  }
-
-  public replaceState(urlObject: HistoryObject): void {
-    this.history = [...this.history.slice(0, this.currentIndex), urlObject];
-    this.currentIndex = this.length - 1;
-  }
-
-  private limited(range: number): number {
-    if (range < -1) {
-      return -1;
-    } else if (range > this.length - 1) {
-      return this.length - 1;
-    } else {
-      return range;
+const backButton = createElement({
+  tagName: 'button',
+  children: ['←'],
+  attributes: {
+    title: '后退'
+  },
+  events: {
+    click(e: MouseEvent) {
+      myHistory.back();
     }
   }
+});
 
-}
+const forwardButton = createElement({
+  tagName: 'button',
+  children: ['→'],
+  attributes: {
+    title: '前进'
+  },
+  events: {
+    click(e: MouseEvent) {
+      myHistory.forward();
+    }
+  }
+});
 
-const historyObj = new MyHistory();
-historyObj;
+const resetButton = createElement({
+  tagName: 'button',
+  children: ['۞'],
+  attributes: {
+    title: '刷新'
+  },
+  events: {
+    click(e: MouseEvent) {
+      myHistory.reset();
+    }
+  }
+});
+
+const address = createElement({
+  tagName: 'input',
+  children: [''],
+  attributes: {
+    placeholder: '请输入一个地址',
+    name: 'address'
+  }
+});
+
+const submit = createElement({
+  tagName: 'button',
+  children: ['✎'],
+  attributes: {
+    type: 'submit'
+  }
+});
+
+const form = createElement({
+  tagName: 'form',
+  children: [address, submit],
+  events: {
+    submit(e: any) {
+      e.preventDefault();
+      const { value } = e.target.elements['address'];
+      myHistory.pushState({
+        url: value,
+        title: null,
+        state: null
+      });
+    }
+  }
+});
+
+const showHistory = createElement({
+  tagName: 'button',
+  children: ['∑'],
+  attributes: {
+    title: 'show'
+  },
+  events: {
+    click(e: MouseEvent) {
+      console.log(myHistory);
+    }
+  }
+});
+
+const addressContainer = createElement({
+  tagName: 'div',
+  children: [backButton, forwardButton, resetButton, form, showHistory],
+  attributes: {
+    class: 'container'
+  }
+});
+
+document.body.appendChild(addressContainer);
+
+myHistory.listenPopState().subscribe((res: HistoryObject) => {
+  (address as HTMLInputElement).value = res.url;
+});
---
+++ services/my-history.ts
@@ -0,0 +1,78 @@
+import { Subject } from 'rxjs';
+
+export interface HistoryObject {
+  url: string;
+  title?: string;
+  state?: { [key: string]: any };
+}
+
+export class MyHistory {
+
+  private history: HistoryObject[] = [];
+  private currentIndex: number = -1;
+  private initialHistory: HistoryObject = {
+    url: '',
+    title: null,
+    state: null
+  };
+
+  private popEvent = new Subject<HistoryObject>();
+
+  get length() {
+    return this.history.length;
+  }
+
+  get currentUrl() {
+    return this.currentIndex === -1
+      ? this.initialHistory.url
+      : this.history[this.currentIndex].url;
+  }
+
+  constructor() {}
+
+  public reset(): void {
+    this.history = [];
+    this.currentIndex = -1;
+  }
+
+  public go(link: number): void {
+    const result = this.currentIndex + link;
+    this.currentIndex = this.limited(result);
+  }
+
+  public forward(): void {
+    this.go(1);
+  }
+
+  public back(): void {
+    this.go(-1);
+  }
+
+  public pushState(urlObject: HistoryObject): void {
+    this.history = [...this.history.slice(0, this.currentIndex + 1), urlObject];
+    this.currentIndex = this.length - 1;
+  }
+
+  public replaceState(urlObject: HistoryObject): void {
+    this.history = [...this.history.slice(0, this.currentIndex), urlObject];
+    this.currentIndex = this.length - 1;
+  }
+
+  public listenPopState(): Subject<HistoryObject> {
+    return this.popEvent;
+  }
+
+  private limited(range: number): number {
+    if (range <= -1) {
+      this.popEvent.next(this.initialHistory);
+      return -1;
+    } else if (range > this.length - 1) {
+      this.popEvent.next(this.history[this.length - 1]);
+      return this.length - 1;
+    } else {
+      this.popEvent.next(this.history[range]);
+      return range;
+    }
+  }
+
+}
